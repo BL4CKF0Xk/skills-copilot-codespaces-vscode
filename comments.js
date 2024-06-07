0fe95ff914d3473c0f349fadb4d149c94ace2ec4
Kavindu Sahan <68717754+BL4CKF0Xk@users.noreply.github.com>
2024-06-07 02:15:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,34 @@
 // create web server
-// 1. create web server
-// 2. create a route
-// 3. create a response
-// 4. send response
-// 5. listen to the port
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
 
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+// create socket server
+var io = require('socket.io').listen(server);
 
-const server = http.createServer((req, res) => {
-    const pathName = url.parse(req.url, true).pathname;
-    const name = url.parse(req.url, true).query.name;
-    const comment = url.parse(req.url, true).query.comment;
+// create comments array
+var comments = [];
 
-    if (pathName === '/comment') {
-        fs.readFile(`${__dirname}/comments.json`, 'utf-8', (err, data) => {
-            if (err) {
-                console.log(err);
-                res.end('Error');
-            } else {
-                const comments = JSON.parse(data);
-                comments.comments.push({ name, comment });
-                fs.writeFile(`${__dirname}/comments.json`, JSON.stringify(comments), (err) => {
-                    if (err) {
-                        console.log(err);
-                        res.end('Error');
-                    } else {
-                        res.end('Success');
-                    }
-                });
-            }
-        });
-    } else {
-        res.end('Invalid Request');
-    }
+// serve comments.json
+app.get('/comments.json', function(req, res){
+  res.json(comments);
 });
 
-server.listen(3000, '
+// serve index.html
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+// listen for new connections
+io.sockets.on('connection', function(socket){
+  // listen for new comments
+  socket.on('send comment', function(data){
+    comments.push(data);
+    io.sockets.emit('comment added', data);
+  });
+});
+
+// listen on port 3000
+server.listen(3000);
+console.log('Server listening on port 3000');
